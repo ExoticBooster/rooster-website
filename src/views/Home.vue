2b721532b9580b5d40058fa1d3767f2b5e35aa5b
julian.VM <julianhahn01@gmail.com>
2019-07-06 22:41:15 +0200

    added a header and the first card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout justify-center>
+    <v-flex xs10 sm6 offset-sm3>
+      <v-card class="mt-4">
+        <v-img
+          class="white--text"
+          height="200px"
+          src="https://images.unsplash.com/photo-1543003743-993c0632996e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        >
+          <v-container fill-height fluid>
+            <v-layout fill-height justify-center>
+              <v-flex xs12 align-end flexbox >
+                <span class="headline black--text">Mein Boot und Ich</span>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-img>
+        <v-card-title>
+          <div>
+            <span>Moin Moin</span><br>
+            <span>Ich bin Stefan und werde euch in dieser tollen
+               Karte mich selbst und mein Boot vorstellen!</span>
+          </div>
+        </v-card-title>
+         <v-card-actions>
+          <v-btn flat color="Light Blue">mehr...</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
