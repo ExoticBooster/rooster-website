1d569f5eda2da6b1226b17afa858cd6db9dab5b5
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-07-08 21:36:40 +0200

    Lazy Loading

--- src/router.js
+++ src/router.js
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
-import Details from './views/Details.vue';
 
 Vue.use(Router);
+
+// route level code-splitting
+// this generates a separate chunk (name.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+function loadView(...view) {
+  return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view.join('/')}.vue`);
+}
 
 export default new Router({
   mode: 'history',
@@ -12,12 +17,12 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: loadView('Home'),
     },
     {
       path: '/details/:title',
       name: 'details',
-      component: Details,
+      component: loadView('Details'),
     },
   ],
 });
