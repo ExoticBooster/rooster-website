00d87ab3d528cf81ca6ffb1d96c750930cbc5a6c
Anton Bracke <anton.bracke.com@gmail.com>
2019-07-12 02:04:39 +0200

    added title and icon link to home

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,27 @@
 <template>
-<v-app>
-  <v-toolbar
-      app
-      color="blue darken-3"
-      dark
-      scroll-off-screen
-      scroll-target="#scrolling-techniques">
-    <v-avatar @click=home>
-      <img src="@/assets/RoosterLogo.png">
-    </v-avatar>
-    <v-toolbar-title>Rooster on the Sea</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-side-icon>
-    </v-toolbar-side-icon>
-  </v-toolbar>
-  <v-content>
-    <router-view></router-view>
-  </v-content>
-</v-app>
+  <v-app>
+    <v-toolbar app color="blue darken-3" dark scroll-off-screen scroll-target="#scrolling-techniques">
+      <router-link :to="{ name: 'home' }">
+        <v-avatar>
+          <img src="@/assets/RoosterLogo.png">
+        </v-avatar>
+      </router-link>
+      <v-toolbar-title>
+        <router-link :to="{ name: 'home' }" class="toolbar-title">Rooster on the Sea</router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-side-icon>
+      </v-toolbar-side-icon>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<script>
-export default {
-  methods: {
-    home() {
-      this.$router.push('/');
-    },
-  },
-};
-</script>
-
-<style>
+<style scoped>
+.toolbar-title {
+  color: inherit;
+  text-decoration: inherit;
+}
 </style>
