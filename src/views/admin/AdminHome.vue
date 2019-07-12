193cface5c815aad7e676c278dfb80375da46fc8
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-07-12 13:25:40 +0200

    added admin logout

---
+++ src/views/admin/AdminHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Admin</p>
+    <v-btn @click="logout">Logout</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminHome',
+  methods: {
+    async logout() {
+      await this.$store.dispatch('logout');
+      this.$router.push({ name: 'login' });
+    },
+  },
+};
+</script>
