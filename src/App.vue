1eb6568639f99a6525bc188a1c30d33a09ebaf85
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-07-13 13:26:48 +0200

    added an navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,48 @@
         <router-link :to="{ name: 'home' }" class="toolbar-title">Rooster on the Sea</router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-side-icon>
+      <v-toolbar-side-icon
+      @click.stop ="drawer = !drawer"
+      >
       </v-toolbar-side-icon>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
     </v-content>
+    <v-navigation-drawer
+    style="display:flex;flex-direction:column;"
+    v-model="drawer"
+    temporary
+    app
+    >
+      <v-list class="pt-0"  three-line>
+          <v-list-tile>
+            <v-list-tile-title class="headline"> Home </v-list-tile-title>
+          </v-list-tile>
+          <v-list-tile>
+            <v-list-tile-title class="headline"> About Me </v-list-tile-title>
+          </v-list-tile>
+      </v-list>
+          <v-spacer></v-spacer>
+      <v-list>
+          <v-list-tile>
+            <v-list-tile-title > <h3>Impressum</h3> </v-list-tile-title>
+            <v-list-tile-title > <h3>Datenschutz</h3> </v-list-tile-title>
+          </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .toolbar-title {
