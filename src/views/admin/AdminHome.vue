4e9d384e514f0c85e992555b4401f3a857772d36
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-07-13 23:15:58 +0200

    added a breakpoint for mobile and desktop

--- src/views/admin/AdminHome.vue
+++ src/views/admin/AdminHome.vue
@@ -1,18 +1,88 @@
 <template>
   <div>
-    <p>Admin</p>
-    <v-btn @click="logout">Logout</v-btn>
+  <!-- Desktop Ansicht -->
+  <v-layout row hidden-sm-and-down>
+    <!-- Links sollen alle aktuellen Anrfagen stehen / oder vielleicht auch einfach erst mal alle -->
+    <v-flex lg6 >
+    LINKS
+    </v-flex>
+    <v-flex lg6>
+    <!-- rechts soll das gleiche Kalendermenü wie im details stehen, womit papa seine touren auswählen kann und auf ausgebucht oder frei setzten kann -->
+    RECHTS
+    </v-flex>
+  </v-layout>
+    <!-- Mobil Ansicht -->
+  <v-layout row wrap hidden-md-and-up>
+    <!-- alle anfragen -->
+    <v-flex xs12>
+    oben
+    </v-flex>
+   <!-- Terminplan -->
+    <v-flex xs12>
+      <!-- Für alle touren die wir grade haben -->
+      <v-card >
+
+      </v-card>
+    </v-flex>
+  </v-layout>
   </div>
 </template>
 
 <script>
+import { convertDate, dateEqual } from '@/utils';
+
 export default {
   name: 'AdminHome',
+  data() {
+    return {
+      dialog: false,
+      selectedDate: null,
+    };
+  },
+
+  computed: {
+    events() {
+      if (!this.tour.events) {
+        return [];
+      }
+
+      return Object.keys(this.tour.events).map((key) => {
+        const event = this.tour.events[key];
+        return {
+          highlight: event.booked ? 'red' : 'green',
+          dates: { start: convertDate(event.start), end: convertDate(event.end) },
+        };
+      });
+    },
+
+    eventsDate() {
+      if (!this.selectedDate) {
+        return [];
+      }
+      return Object.keys(this.tour.events)
+        .filter(i => dateEqual(this.tour.events[i].start, this.selectedDate))
+        .map(i => this.tour.events[i]);
+    },
+  },
+
   methods: {
     async logout() {
       await this.$store.dispatch('logout');
       this.$router.push({ name: 'login' });
     },
+
+    loadContent() {
+      this.$store.dispatch('loadTour', this.id);
+    },
+    showTours(days) {
+      this.dialog = true;
+      this.selectedDate = days.date;
+    },
+
+  },
+
+  filters: {
+    date: convertDate,
   },
 };
 </script>
