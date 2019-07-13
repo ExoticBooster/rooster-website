005a1aa40e284cf93d5bd36713b546fdb0eed07b
Anton Bracke <anton.bracke.com@gmail.com>
2019-07-13 20:55:23 +0200

    added matomo tracking

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import './firebase';
 import Vue from 'vue';
 import VCalendar from 'v-calendar';
 import Vuetify from 'vuetify';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './registerServiceWorker';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import tracking from '@/tracking';
+import '@/registerServiceWorker';
+import '@/firebase';
+
 import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
 
 Vue.config.productionTip = false;
@@ -19,3 +21,5 @@ new Vue({
   store,
   render(h) { return h(App); },
 }).$mount('#app');
+
+tracking.init(router);
