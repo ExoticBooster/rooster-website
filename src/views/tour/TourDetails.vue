7ebd00152f2b296738226bb4c08824516ce39196
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-07-13 11:39:11 +0200

    added the events dialog and dates

--- src/views/tour/TourDetails.vue
+++ src/views/tour/TourDetails.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-layout class="justify-center">
-    <v-flex xs10 sm6>
-      <v-card v-if="tour" class="mt-4">
+  <v-layout justify-center>
+    <v-flex xs12 sm10 lg6>
+      <v-card v-if="tour" class="pb-3" :class="{ 'mt-0': $vuetify.breakpoint.smAndDown, 'mt-5': $vuetify.breakpoint.mdAndUp }">
         <!-- Card Titel -->
         <v-card-title>
           <h2>{{ tour.title }}</h2>
         </v-card-title>
         <!-- Card Image -->
-        <v-img :src="tour.cover" class="rounded" />
+        <v-img :src="tour.cover" class="mx-3 rounded" />
         <!-- Card Subtitel -->
         <v-card-title v-if="tour.subtitle" class="justify-center pa-0 pt-2">
           <h4>{{ tour.subtitle }}</h4>
@@ -22,33 +22,77 @@
             #TODO:
             - Dont load the pictures in full resulution just for Thumbnails!
          -->
-        <v-card-title v-if="tour.imgs && tour.imgs.length > 0" class="justify-start mb-0 ml-4">
-          Impressionen:
-        </v-card-title>
-        <v-layout class="justify-center mb-2 mt-0">
-          <v-avatar v-for="img in tour.imgs" :key="img" class="ml-3">
-            <v-img :src="img" />
-          </v-avatar>
-        </v-layout>
-        <!-- Gallery Button -->
-        <v-card-actions v-if="tour.imgs && tour.imgs.length > 0" class="justify-center mb-5">
+        <v-flex v-if="tour.imgs && tour.imgs.length > 0" justify-center class="mt-2 mb-4 text-xs-center">
+          <h3>Impressionen</h3>
+          <v-flex class="my-3">
+            <v-avatar v-for="img in tour.imgs" :key="img" class="mx-1">
+              <v-img :src="img" />
+            </v-avatar>
+          </v-flex>
           <v-btn :to="{ name: 'tourGallery', params: { id: tour.id } }">Zur Galerie</v-btn>
-        </v-card-actions>
+        </v-flex>
         <!-- Card Callendar -->
-        <v-card-actions class="justify-center">
-          <vc-calendar title-position="left" :attributes="events" />
-        </v-card-actions>
-
-        <router-link :to="{ name: 'tour', params: { id: 'QBUxNLEwIYH9od2o0C5m' }}">trave</router-link>
+        <v-flex mx-3>
+          <vc-calendar title-position="left" is-expanded :attributes="events" @dayclick="showTours"/>
+        </v-flex>
       </v-card>
       <v-progress-circular v-else :width="5" color="primary" indeterminate />
     </v-flex>
+
+    <v-dialog v-if="tour" v-model="dialog" persistent>
+      <v-card>
+        <v-card-title class="headline grey lighten-2" primary-title>Touren - {{ selectedDate | date('normal') }} </v-card-title>
+        <template v-if="tour.events && Object.keys(tour.events).length > 0">
+          <v-list two-line subheader class="">
+            <v-list-tile v-for="(event, key) in tour.events" :key="key" class="pa-0" style="border-bottom: 1px solid #bbb">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ event.title }}</v-list-tile-title>
+                <v-list-tile-sub-title>Start: {{ event.start | date('short') }} {{ event.startTime }}</v-list-tile-sub-title>
+                <v-list-tile-sub-title>Ende: {{ event.end | date('short') }} {{ event.endTime }}</v-list-tile-sub-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-btn v-if="event.booked" depressed small color="error">ausgebucht</v-btn>
+                <v-btn v-else :to="{ name: 'tourBooking', params: { id, event: key }}" small color="success">buchen</v-btn>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </template>
+        <v-card-text v-else>
+          <span>Am 28.7.2019 bieten wir leider keine Touren an. Wähle Sie gerne einen anderen Tag.</span>
+        </v-card-text>
+        <v-card-actions class="mt-auto">
+          <v-spacer></v-spacer>
+          <v-btn color="primary" flat @click="dialog=false">Schliessen</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 
 <script>
+import moment from 'moment';
+
+function convertDate(value, format) {
+  if (format === 'short') {
+    return moment(value).format('DD.MM');
+  }
+
+  if (format === 'normal') {
+    return moment(value).format('DD.MM.YYYY');
+  }
+
+  // Für Kalendar MM.DD.YYYY
+  return moment(value).toDate();
+}
+
 export default {
   name: 'TourDetails',
+  data() {
+    return {
+      dialog: false,
+      selectedDate: null,
+    };
+  },
   computed: {
     id() {
       return this.$route.params.id;
@@ -61,14 +105,18 @@ export default {
         return [];
       }
 
-      return this.tour.events.map(i => ({
-        highlight: i.booked ? 'red' : 'blue',
-        dates: new Date(this.convertDate(i.start)),
-        popover: {
-          label: i.title,
-          visibility: 'click',
-        },
-      }));
+      return Object.keys(this.tour.events).map((key) => {
+        const event = this.tour.events[key];
+        return {
+          highlight: event.booked ? 'red' : 'green',
+          dates: { start: convertDate(event.start), end: convertDate(event.end) },
+        };
+      });
+    },
+
+    //was sollte hier rein? war vorher leer
+    eventsDate() {
+      return 'hallo';
     },
   },
   watch: {
@@ -76,16 +124,18 @@ export default {
       this.loadContent();
     },
   },
+  filters: {
+    date: convertDate,
+  },
   methods: {
     loadContent() {
       this.$store.dispatch('loadTour', this.id);
     },
-    convertDate(date) {
-      const parts = date.split('.');
-      return ` ${parts[1]}.${parts[0]}.${parts[2]} `;
+    showTours(days) {
+      this.dialog = true;
+      this.selectedDate = days.date;
     },
   },
-
   beforeMount() {
     this.loadContent();
   },
@@ -94,6 +144,6 @@ export default {
 
 <style scoped>
 .rounded{
-  border-radius:15px;
+  border-radius: 5px;
 }
 </style>
