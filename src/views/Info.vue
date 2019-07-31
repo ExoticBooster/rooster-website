197514dee9790e33889e20a7fa55e8aedd58ded3
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-07-31 19:01:27 +0200

    new created

---
+++ src/views/Info.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-layout row wrap justify-center>
+        <v-flex xs10 sm12 md12 lg12>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+
+</style>
