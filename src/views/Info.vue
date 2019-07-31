25ba5473e43d62acdda018278dcac6442a4a3932
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-07-31 21:40:55 +0200

    created Title on info and cards to create on admin

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,6 +1,10 @@
 <template>
     <v-layout row wrap justify-center>
-        <v-flex xs10 sm12 md12 lg12>
+        <v-flex xs10 sm12 md12 lg12 class="mt-4 text-md-center">
+            <h1>Fragen und Antworten</h1>
+        </v-flex>
+        <v-flex md5 offset-md1>
+            <h2></h2>
         </v-flex>
     </v-layout>
 </template>
@@ -12,5 +16,16 @@ export default {
 </script>
 
 <style scoped>
+h1{
+    font-family: 'Karla', sans-serif;
+    color: #607D8B;
+    font-size: 50px;
+};
+
+h2{
+    font-family: 'Karla', sans-serif;
+    color: #607D8B;
+    font-size: 30px;
+};
 
 </style>
--- src/views/admin/AdminTexts.vue
+++ src/views/admin/AdminTexts.vue
@@ -1,6 +1,30 @@
 <template>
-  <v-layout>
-    AdminTexts
+  <v-layout row wrap>
+    <v-flex lg5 offset-lg1>
+
+    </v-flex>
+    <v-flex lg5 offset-lg1>
+      <v-layout row wrap>
+        <v-flex lg12 class="mt-2">
+          <h2>Frage und Antwort erstellen:</h2>
+        </v-flex>
+        <v-flex md10 lg10 class="mt-5">
+          <h3>Frage:</h3>
+          <v-textarea box>
+          </v-textarea>
+        </v-flex>
+        <v-flex md10 lg10 class="mt-5">
+          <h3>Antowort:</h3>
+          <v-textarea box>
+          </v-textarea>
+        </v-flex>
+        <v-flex offset-lg4>
+          <v-btn color="success">
+            Speichern
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-flex>
   </v-layout>
 </template>
 
