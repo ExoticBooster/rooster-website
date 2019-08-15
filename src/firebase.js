17dbb458a158c9d8f92d740d20a1cf0a174aaa43
Julian Hahn <40185075+ExoticBooster@users.noreply.github.com>
2019-08-15 15:06:31 +0200

    linted the config data from " to '

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBx6L2JeXbt51Unc-djenk-iAyRUKprZtc",
-  authDomain: "rooster-4faf7.firebaseapp.com",
-  databaseURL: "https://rooster-4faf7.firebaseio.com",
-  projectId: "rooster-4faf7",
-  storageBucket: "rooster-4faf7.appspot.com",
-  messagingSenderId: "985634018262",
-  appId: "1:985634018262:web:097748c628a4c107"
+  apiKey: 'AIzaSyBx6L2JeXbt51Unc-djenk-iAyRUKprZtc',
+  authDomain: 'rooster-4faf7.firebaseapp.com',
+  databaseURL: 'https://rooster-4faf7.firebaseio.com',
+  projectId: 'rooster-4faf7',
+  storageBucket: 'rooster-4faf7.appspot.com',
+  messagingSenderId: '985634018262',
+  appId: '1:985634018262:web:097748c628a4c107',
 });
 
 const storage = firebase.storage();
